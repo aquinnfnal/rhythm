b09b487d3cea007a9609512b5e447b71fd25f157
Adam Quinn <aquinn@fnal.gov>
2026-03-26 08:59:30 -0500

    Updated sim directory to include username

--- rhythm_globals.py
+++ rhythm_globals.py
@@ -1,2 +1,8 @@
 
 GLOBAL_SKIP_SIMULATION = False
+
+
+# Simulation and netlisting files will be written to:
+# {DEFAULT_SIM_DIRECTORY}/{username}/{tb.rundir}
+
+DEFAULT_SIM_DIRECTORY = "/asic_sim/rhythm"
